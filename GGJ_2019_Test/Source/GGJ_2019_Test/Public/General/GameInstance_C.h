86737ec21e5e14976909356f56e9524d462cefa5
Troy Maynard <troyvmaynard@gmail.com>
2019-01-26 18:47:49 -0500

    Enemies, HP Bars Etc.

---
+++ GGJ_2019_Test/Source/GGJ_2019_Test/Public/General/GameInstance_C.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "GameInstance_C.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ_2019_TEST_API UGameInstance_C : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
